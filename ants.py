ead8fe617bca4f6e5a95e4e1ee9d827188529e36
Sakuragi-crypto <108550029+Sakuragi-crypto@users.noreply.github.com>
2023-02-27 17:12:19 +0100

    pheromones

--- ants.py
+++ ants.py
@@ -1,3 +1,7 @@
+import networkx as nx
+import itertools
+
+
 class Ant():
 
     def __init__(self, starting_point, objectif_point):
@@ -12,3 +16,12 @@ class Ant():
 
     def has_finished(self):
         return self.objectif == self.solution[-1]
+
+    def init_pheromones(self, graph):
+        pheromones = nx.Graph()
+        leaves = graph.nodes
+        pheromones.add_nodes_from(graph.nodes)
+        for edge in list(itertools.combinations(leaves, 2)):
+            pheromones.add_edge(edge[0], edge[1], weight=1)
+        return pheromones
+
