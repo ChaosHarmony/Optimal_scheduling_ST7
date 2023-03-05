a7e64ff86a96f5ae5a15f53f89be2f3e76417f33
ChaosHarmony <harmonie.lebrun1@gmail.com>
2023-03-05 15:54:13 +0100

    add generalized func + get_phero

--- AntColony.py
+++ AntColony.py
@@ -5,11 +5,15 @@ import graph_operation as go
 
 
 class AntColony():
-    def __init__(self, DAG_scheduling: nx.DiGraph, n_ants: int, start_point: str or int = 'start', objectif_point: str or int = 'end'):
+    def __init__(self, DAG_scheduling: nx.DiGraph, n_ants: int, ants: list):
         self.directed_graph = DAG_scheduling
         self.complete_graph = go.generate_complete_graph(DAG_scheduling)
-        self.colony_list = [ants.Ant(start_point, objectif_point)
-                            for i in range(n_ants)]
+        self.colony_list = ants
+        self.n_ants = n_ants
+
+    ####################################################################
+    # Pheromon part
+    #################################################################
 
     def init_pheromones(self):
         leaves = self.complete_graph.nodes()
@@ -17,15 +21,41 @@ class AntColony():
             self.complete_graph.update(
                 (edge[0], edge[1], {'pheromon trail': 1.}))
 
-    def update_pheromones(self, edge: tuple, pheromon_value: float):
+    def get_pheromon(self, u, v):
         """
-        edge is a tuple (u,v) of the verteces u and v to designat the edge between u and v in
+        the verteces u and v designate the edge between u and v in
+        the complete graph
+        """
+        return self.complete_graph.edges[u, v]['pheromon trail']
+
+    def update_pheromones(self, u, v, pheromon_value: float):
+        """
+        edge is a tuple (u,v) of the verteces u and v to designate the edge between u and v in
         the complete graph
         pheromon_value will be ADD (+) to the original value for instance :
         pheromon = pheromon - P -> pheromon_value = -P
         in place modification on the complete_graph
         """
-        u, v = edge
-        new_value = self.complete_graph.edges[u,
-                                              v]['pheromon trail']
+        new_value = self.get_pheromon(u, v) - pheromon_value
         self.complete_graph.update((u, v, {'pheromon trail': new_value}))
+
+    #################################################################
+    # Generalized ant operations
+    #################################################################
+
+    def get_solution_list(self):
+        return [self.colony_list[i].solution for i in range(self.n_ants)]
+
+    def get_machine_attribution_list(self):
+        """
+        Only works if ants are Ant_TGE type
+        """
+        try:
+            type(self.colony_list[0]) == type(ants.Ant_TGE)
+        except:
+            raise Exception("Ants are of wrong type")
+
+        return [self.colony_list[i].affected_machine for i in range(self.n_ants)]
+
+    ##################################################################
+    #
