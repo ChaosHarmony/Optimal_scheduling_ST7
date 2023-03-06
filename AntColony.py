9a01e374d4948c6e0178ef4cb482b93902becc3f
ChaosHarmony <harmonie.lebrun1@gmail.com>
2023-03-06 14:36:41 +0100

    push needed

--- AntColony.py
+++ AntColony.py
@@ -2,14 +2,15 @@ import ants
 import networkx as nx
 import itertools
 import graph_operation as go
+import matplotlib.pyplot as plt
 
 
 class AntColony():
-    def __init__(self, DAG_scheduling: nx.DiGraph, n_ants: int, ants: list):
+    def __init__(self, DAG_scheduling: nx.DiGraph, ants: list):
         self.directed_graph = DAG_scheduling
         self.complete_graph = go.generate_complete_graph(DAG_scheduling)
         self.colony_list = ants
-        self.n_ants = n_ants
+        self.n_ants = len(ants)
 
     ####################################################################
     # Pheromon part
@@ -18,8 +19,9 @@ class AntColony():
     def init_pheromones(self):
         leaves = self.complete_graph.nodes()
         for edge in list(itertools.combinations(leaves, 2)):
+            print("inside")
             self.complete_graph.update(
-                (edge[0], edge[1], {'pheromon trail': 1.}))
+                [(edge[0], edge[1], {'pheromon trail': 1.})])
 
     def get_pheromon(self, u, v):
         """
@@ -59,3 +61,28 @@ class AntColony():
 
     ##################################################################
     #
+
+
+if __name__ == "__main__":
+    print("testing range")
+    print("===================================================")
+    graph_path = "./Graphs/smallRandom.json"
+    print("getting graph from ", graph_path, " ...")
+    graph, graph_nodes = go.extract_directed_graph(graph_path)
+    print("starting graph transformation")
+    go.transform(graph, graph_nodes)
+    new_nodes = [*graph_nodes, "start", "end"]
+
+    colony = AntColony(graph, ants=[ants.Ant(
+        'start', 'end') for i in range(10)])
+    print("colony is based on 10 ants : ", colony.n_ants)
+    print("we should have created a complete garph while initializing the colony")
+    plt.tight_layout()
+    nx.draw_networkx(colony.complete_graph, arrows=False)
+    plt.show()
+
+    print('complete graph edeges are ', colony.complete_graph.edges)
+
+    print('initialzing pheromon, should update the complete graph')
+    colony.init_pheromones()
+    print("pheromones added : ", colony.complete_graph.edges)
--- ants.py
+++ ants.py
@@ -18,14 +18,6 @@ class Ant():
     def has_finished(self):
         return self.objectif == self.solution[-1]
 
-    def init_pheromones(self, graph):
-        pheromones = nx.Graph()
-        leaves = graph.nodes
-        pheromones.add_nodes_from(graph.nodes)
-        for edge in list(itertools.combinations(leaves, 2)):
-            pheromones.add_edge(edge[0], edge[1], weight=1)
-        return pheromones
-
 
 class Ant_TGE(Ant):
     def __init__(self, starting_point, objectif_point, nb_machines):
