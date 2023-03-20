20907fdf3825c912805d0fa7c80671b4dbd21542
ChaosHarmony <harmonie.lebrun1@gmail.com>
2023-03-20 15:36:52 +0100

    checkout needed

--- main.py
+++ main.py
@@ -10,27 +10,30 @@ from graph_functions import *
 from time import process_time
 
 
-DAG = create_DAG(import_graph("Graphs/xsmallComplex.json"))
+DAG = create_DAG(import_graph("Graphs/smallRandom.json"))
 print(DAG)
 
 # give result in hour.
 
 
-
 print("Basic Ants")
 
-basic_ant_start = process_time() 
-best_makespan, best_schedule, iterations_results = ACO_basic_ants(graph=DAG, num_ants = 100, num_iterations = 10)
+basic_ant_start = process_time()
+best_makespan, best_schedule, iterations_results = ACO_basic_ants(
+    graph=DAG, num_ants=1000, num_iterations=100)
 basic_ant_end = process_time()
 print(best_makespan/3600)
 # print(best_schedule)
 print(f"Elapsed time (CPU): {basic_ant_end-basic_ant_start}s")
 # print(list(map(lambda x: x["Makespan"], iterations_results.values())))
-# plt.plot(iterations_results.keys(), list(map(lambda x: x["Makespan"], iterations_results.values())))
+plt.plot(iterations_results.keys(), list(
+    map(lambda x: x["Makespan"], iterations_results.values())))
+plt.show()
 
 print("Elite Ants")
-elite_ant_start = process_time() 
-best_makespan, best_schedule, iterations_results = ACO_elite_ants(graph=DAG,num_ants = 100, num_iterations = 10)
+elite_ant_start = process_time()
+best_makespan, best_schedule, iterations_results = ACO_elite_ants(
+    graph=DAG, num_ants=100, num_iterations=10)
 elite_ant_end = process_time()
 print(best_makespan/3600)
 # print(best_schedule)
@@ -39,4 +42,3 @@ print(f"Elapsed time (CPU): {elite_ant_end-elite_ant_start}s")
 
 
 # plt.plot(iterations_results.keys(), list(map(lambda x: x["Makespan"], iterations_results.values())))
-# plt.show()
