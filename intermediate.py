4016b64e81eab398a5b75ae6b775de0729d1f4fd
ChaosHarmony <harmonie.lebrun1@gmail.com>
2023-03-22 15:36:29 +0100

    changed main form

---
+++ intermediate.py
@@ -0,0 +1,82 @@
+
+import numpy as np
+import networkx as nx
+import json
+import matplotlib.pyplot as plt
+from Job import Job
+from Machine import Machine
+from ACO import *
+from visibility_func import *
+from graph_functions import *
+from time import process_time
+from mpi4py import MPI
+
+
+def visibility_choice(parameters):
+    global visibility_func
+    if parameters["visibility function"] == "process":
+        visibility_func = procces_visibilty_func
+    elif parameters["visibility function"] == "child and process":
+        visibility_func = greater_work_free
+    else:
+        print("visibility function doesn't exist, default to greater work free")
+        visibility_func = greater_work_free
+
+
+def resolution(parameters):
+    print("\n==============================================================================\n")
+    comm = MPI.COMM_WORLD
+    print("hello main.py from : ", comm.Get_rank(), ' \n')
+
+    DAG = create_DAG(import_graph(parameters["DAG_path"]))
+    print(DAG)
+
+    visibility_choice(parameters)
+
+    # give result in hour.
+    if parameters["Ants type"] == "Basic Ants":
+        print("Basic Ants")
+
+        basic_ant_start = process_time()
+        local_best_makespan, local_best_schedule, local_iterations_results = ACO_basic_ants(
+            graph=DAG, num_machines=parameters["machines number"], num_ants=parameters[
+                "ants number"], alpha=parameters["alpha"],
+            beta=parameters["beta"], evaporation_rate=parameters["evaporation"], q=parameters["Q"], visibility_function=visibility_func,
+            num_iterations=parameters["iteration number"])
+        basic_ant_end = process_time()
+        print("best makepan of process #{0} : {1}h ".format(
+            comm.Get_rank(), local_best_makespan))
+        # print(best_schedule)
+        print(
+            f"Elapsed time (CPU) for process#{comm.Get_rank()}: {basic_ant_end-basic_ant_start}s")
+        # print(list(map(lambda x: x["Makespan"], iterations_results.values())))
+        iterations_results_list = comm.gather(local_iterations_results, root=0)
+        print("\n==============================================================================\n")
+        if rank == 0:
+            print(
+                "\n==============================================================================\n")
+            print("gathering of final solutions")
+            iterations_results: dict = iterations_results_list[0]
+            for i in range(1, comm.Get_size()):
+                for key in iterations_results.keys():
+                    if iterations_results_list[i][key]["Makespan"] < iterations_results[key]["Makespan"]:
+                        iterations_results[key] = iterations_results_list[i][key]
+
+            plt.plot(iterations_results.keys(), list(
+                map(lambda x: x["Makespan"], iterations_results.values())))
+            plt.show()
+            print(
+                "\n==============================================================================\n")
+        print("\n====================================      END      ===============================\n")
+    if parameters["Ants type"] == "Elite Ants":
+        print("Elite Ants")
+        elite_ant_start = process_time()
+        best_makespan, best_schedule, iterations_results = ACO_elite_ants(
+            graph=DAG, num_ants=100, num_iterations=10)
+        elite_ant_end = process_time()
+        print(best_makespan/3600)
+        # print(best_schedule)
+        print(f"Elapsed time (CPU): {elite_ant_end-elite_ant_start}s")
+        # print(list(map(lambda x: x["Makespan"], iterations_results.values())))
+
+        # plt.plot(iterations_results.keys(), list(map(lambda x: x["Makespan"], iterations_results.values())))
--- main.py
+++ main.py
@@ -1,64 +1,34 @@
+from intermediate import resolution
 
-import numpy as np
-import networkx as nx
-import json
-import matplotlib.pyplot as plt
-from Job import Job
-from Machine import Machine
-from ACO import *
-from graph_functions import *
-from time import process_time
-from mpi4py import MPI
+parameters = {
+    "DAG_path":
+    # "./Graphs/smallComplex.json",
+    "./Graphs/mediumRandom.json",
 
-print("\n==============================================================================\n")
-comm = MPI.COMM_WORLD
-print("hello main.py from : ", comm.Get_rank(), ' \n')
-
-DAG = create_DAG(import_graph("Graphs/mediumRandom.json"))
-print(DAG)
-
-# give result in hour.
+    "Ants type":
+    "Basic Ants",
+    # "Elite Ants",
 
 
-print("Basic Ants")
+    "machines number":
+    2,
+    "ants number":
+    1000,
 
-basic_ant_start = process_time()
-local_best_makespan, local_best_schedule, local_iterations_results = ACO_basic_ants(
-    graph=DAG, num_ants=1000, num_iterations=100)
-basic_ant_end = process_time()
-print("best makepan of process #{0} : {1}h ".format(
-    comm.Get_rank(), local_best_makespan))
-# print(best_schedule)
-print(
-    f"Elapsed time (CPU) for process#{comm.Get_rank()}: {basic_ant_end-basic_ant_start}s")
-# print(list(map(lambda x: x["Makespan"], iterations_results.values())))
-iterations_results_list = comm.gather(local_iterations_results, root=0)
-print("\n==============================================================================\n")
-if rank == 0:
-    print("\n==============================================================================\n")
-    print("gathering of final solutions")
-    iterations_results: dict = iterations_results_list[0]
-    for i in range(1, comm.Get_size()):
-        for key in iterations_results.keys():
-            if iterations_results_list[i][key]["Makespan"] < iterations_results[key]["Makespan"]:
-                iterations_results[key] = iterations_results_list[i][key]
+    "iteration number":
+    100,
 
-    plt.plot(iterations_results.keys(), list(
-        map(lambda x: x["Makespan"], iterations_results.values())))
-    plt.show()
-    print("\n==============================================================================\n")
-    print("\n====================================      END      ===============================\n")
-'''
-print("Elite Ants")
-elite_ant_start = process_time()
-best_makespan, best_schedule, iterations_results = ACO_elite_ants(
-    graph=DAG, num_ants=100, num_iterations=10)
-elite_ant_end = process_time()
-print(best_makespan/3600)
-# print(best_schedule)
-print(f"Elapsed time (CPU): {elite_ant_end-elite_ant_start}s")
-# print(list(map(lambda x: x["Makespan"], iterations_results.values())))
+    # HYPERPARAM
+    "alpha":
+    1.0,
+    "beta":
+    2.0,
+    "evaporation":
+    0.2,
+    "Q":
+    1,
+    "visibility function":
+    "process"
+    # "child and process"
 
-
-# plt.plot(iterations_results.keys(), list(map(lambda x: x["Makespan"], iterations_results.values())))
-'''
+}
