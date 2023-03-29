9ca79c2f0a8fb6146dd5f4ab3bea710bc1876b43
ChaosHarmony <123765874+ChaosHarmony@users.noreply.github.com>
2023-03-29 18:36:01 +0200

    Update main.py
    forgot '/'

--- main.py
+++ main.py
@@ -63,13 +63,13 @@ parameters = {
 
 print("Testing for several Q values")
 Q_test = [0.001, 0.01, 0.1, 1, 10, 100]
-parameters["repo"] = "./results/Q_param"
+parameters["repo"] = "./results/Q_param/"
 for Q in Q_test:
     parameters["Q"] = Q
     resolution(parameters)
 print("====================== END of Q =============================")
 print("\n Testing with 3Q, different alpha and beta bias values")
-parameters["repo"] = "./results/Bias_param"
+parameters["repo"] = "./results/Bias_param/"
 alpha_beta = [(1, 1), (0.5, 1), (0.1, 1), (0.1, 0.5),
               (0.5, 0.1), (1, 0.1), (1, 2), (2, 1)]
 for alpha, beta in alpha_beta:
@@ -80,7 +80,7 @@ for alpha, beta in alpha_beta:
         resolution(parameters)
 print("========================= END of alpha beta ====================")
 print("\n testing differente evaporations")
-parameters["repo"] = "./results/evap"
+parameters["repo"] = "./results/evap/"
 evap = [0.1, 0.2, 0.25, 0.3, 0.5, 0.7]
 parameters["alpha"] = 1
 parameters["beta"] = 2
@@ -89,7 +89,7 @@ for rho in evap:
     parameters["evaporation"] = rho
     resolution(parameters)
 print("======================= END of evap ==========================")
-parameters["repo"] = "resluts/C_param"
+parameters["repo"] = "resluts/C_param/"
 C = [1, 10, 50, 100]
 parameters["Q"] = 0.1
 parameters["evaporation"] = 0.3
